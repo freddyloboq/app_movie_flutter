d11ef3ad2f90a281f9c36a47fb493febc99d3582
Freddy Lobo <freddyloboq@gmail.com>
2022-06-22 22:17:50 -0400

    New app movie

---
+++ android/app/src/main/kotlin/com/example/app_movies_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_movies_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/screens.dart
@@ -0,0 +1,2 @@
+export 'package:app_movies_flutter/screens/dtails_screen.dart';
+export 'package:app_movies_flutter/screens/home_screen.dart';

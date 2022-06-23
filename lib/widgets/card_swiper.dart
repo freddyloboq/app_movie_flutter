96c665584ba1e22c59fa6b70070d2041f5e46a50
Freddy Lobo <freddyloboq@gmail.com>
2022-06-22 23:37:27 -0400

    New app movie

--- lib/widgets/card_swiper.dart
+++ lib/widgets/card_swiper.dart
@@ -10,17 +10,17 @@ class CardSwiperScreen extends StatelessWidget {
       width: double.infinity,
       height: size.height * 0.5,
       // color: Colors.red,
-      child: GestureDetector(
-        onTap: () => Navigator.pushNamed(context, 'details',
-            arguments: 'movie-instance'),
-        child: Swiper(
-          itemCount: 10,
-          layout: SwiperLayout.STACK,
-          itemWidth: size.width * 0.7,
-          itemHeight: size.height * 0.4,
-          // itemBuilder: (BuildContext context, int index){
-          itemBuilder: (_, int index) {
-            return ClipRRect(
+      child: Swiper(
+        itemCount: 10,
+        layout: SwiperLayout.STACK,
+        itemWidth: size.width * 0.7,
+        itemHeight: size.height * 0.4,
+        // itemBuilder: (BuildContext context, int index){
+        itemBuilder: (_, int index) {
+          return GestureDetector(
+            onTap: () => Navigator.pushNamed(context, 'details',
+                arguments: 'movie-instance'),
+            child: ClipRRect(
               borderRadius: BorderRadius.circular(20),
               child: const FadeInImage(
                 // placeholder: NetworkImage('https://via.placeholder.com/300x400'),
@@ -28,9 +28,9 @@ class CardSwiperScreen extends StatelessWidget {
                 image: NetworkImage('https://via.placeholder.com/300x400'),
                 fit: BoxFit.cover,
               ),
-            );
-          },
-        ),
+            ),
+          );
+        },
       ),
     );
   }
